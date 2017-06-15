6692330de3c50cdef7862463d94ae42fda3d2625
DESKTOP-O8GP7PU\David <davidflom01@gmail.com>
2017-06-15 06:08:15 -0400

    inititial commit

---
+++ app/src/main/java/vikingchess/userinterfacechapter3/MainActivity.java
@@ -0,0 +1,57 @@
+package vikingchess.userinterfacechapter3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.graphics.Color.BLUE;
+
+public class MainActivity extends AppCompatActivity {
+    // auto complete list data
+    String[] continents = {"North America", "South America", "Europe", "Asia", "Africa", "Antarctica"};
+    // Declare animal variable
+    EditText favoriteAnimal;
+    TextView answerText;
+    TextView continentAnswer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // Create Auto complete list
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, continents);
+        AutoCompleteTextView actv = (AutoCompleteTextView) findViewById(R.id.continent);
+        actv.setThreshold(1);
+        actv.setAdapter(adapter);
+        actv.setTextColor(BLUE);
+
+        // Adding function to the button
+        Button continentButton = (Button) this.findViewById(R.id.continentButton);
+        continentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO This function needs refractoring
+                //TODO put away keyboard
+                // Grab animal name
+                favoriteAnimal = (EditText) findViewById(R.id.favoriteAnimalText);
+                String FavoriteAnimalString=favoriteAnimal.getText().toString();
+                answerText = (TextView) findViewById(R.id.answerTextView);
+                // Grab Continent name
+                continentAnswer = (TextView) findViewById(R.id.continent);
+                String continenetAnswerText=continentAnswer.getText().toString();
+                //Display answer
+                answerText.setText("Your Favorite animal is a " + FavoriteAnimalString + " resides on the " + continenetAnswerText);
+
+                //TODO Open new map view display continent
+                    //seperate function?
+            }
+        });
+    }
+}
