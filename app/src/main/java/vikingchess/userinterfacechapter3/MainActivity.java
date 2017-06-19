f9f0ad6263257141077f12dad453afe4f8dbae38
DESKTOP-O8GP7PU\David <davidflom01@gmail.com>
2017-06-19 13:33:56 -0400

    Text Activity complete

--- app/src/main/java/vikingchess/userinterfacechapter3/MainActivity.java
+++ app/src/main/java/vikingchess/userinterfacechapter3/MainActivity.java
@@ -7,7 +7,10 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import static android.graphics.Color.BLUE;
 
@@ -18,12 +21,11 @@ public class MainActivity extends AppCompatActivity {
     EditText favoriteAnimal;
     TextView answerText;
     TextView continentAnswer;
+    TextView habitatAnswer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         // Create Auto complete list
         ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, continents);
@@ -31,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
         actv.setThreshold(1);
         actv.setAdapter(adapter);
         actv.setTextColor(BLUE);
+
+        // Create Mutlt auto complate list
+        MultiAutoCompleteTextView habitat = (MultiAutoCompleteTextView)this.findViewById(R.id.habitat);
+        ArrayAdapter<String> aa2 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, new String[]
+                {
+                        "Ocean", "Lake", "Desert", "Rainforest", "Tundra", "Forest", "Mountains", "Wetlands", "Plains"
+                });
+        habitat.setAdapter(aa2);
+        habitat.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+
 
         // Adding function to the button
         Button continentButton = (Button) this.findViewById(R.id.continentButton);
@@ -44,10 +56,15 @@ public class MainActivity extends AppCompatActivity {
                 String FavoriteAnimalString=favoriteAnimal.getText().toString();
                 answerText = (TextView) findViewById(R.id.answerTextView);
                 // Grab Continent name
+
                 continentAnswer = (TextView) findViewById(R.id.continent);
                 String continenetAnswerText=continentAnswer.getText().toString();
+                // Grab habitat
+                habitatAnswer = (TextView) findViewById(R.id.habitat);
+                String habitatAnswerText = habitatAnswer.getText().toString();
                 //Display answer
-                answerText.setText("Your Favorite animal is a " + FavoriteAnimalString + " resides on the " + continenetAnswerText);
+                answerText.setText("Your Favorite animal is a " + FavoriteAnimalString + " it resides on the " + continenetAnswerText
+                 + " continent in a " + habitatAnswerText + "habitat.");
 
                 //TODO Open new map view display continent
                     //seperate function?
